d387febe3a24bd1a99a23d4fe352bb07f62775b9
Bence Rigler <rigbence@gmail.com>
2019-09-18 22:01:48 +0200

    working on the modal1

---
+++ src/components/Modal.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Consumer } from '../context';
+import { ButtonContainer } from './Button';
+import { Link } from 'react-router-dom'; 
+ 
+class Modal extends Component {
+    render(){
+        return(
+            <Consumer>
+                {value => {
+                  const { modalOpen, closeModal} = value;
+                  const {img, title, price} = value.modalProduct
+                  if(!modalOpen){
+                      return null;
+                  } else {
+                      return <ModalContainer>
+                          <div className="container">
+                              <div className="row">
+                                  <div id="modal" className="col-8 mx-auto cold-md-6 col-lg-4 text-center text-capitalize p-5">
+                                      <h5 className="py-4">Item added to the cart</h5>
+                                      <img src={img} className='img-fluid' alt='product'/>
+                                      <h5>{title}</h5>
+                                      <h5 className="text-muted">price : ${price}</h5>
+                                  </div>
+                              </div>
+                          </div>
+                      </ModalContainer> 
+                  }
+                }}
+            </Consumer>
+        )
+    }
+}
+const ModalContainer = styled.div`
+position:fixed;
+top:0;
+left:0;
+right:0;
+bottom:0;
+background:rgba(0,0,0,0.3);
+display:flex;
+align-items:center;
+justify-content:center;
+#modal {
+    background:#fff;
+    }
+`
+export default Modal
